82a0571bf691ce86353cf48b3cf1f53ebef630c5
Amoo89 <aonifadeolanrewaju@gmail.com>
2022-09-09 00:21:41 -0400

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -3,5 +3,5 @@
 wget -P /tmp/ https://raw.github.com/Amoo89/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
 =======
 wget -P /tmp/ https://raw.github.com/Amoo89/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
->>>>>>> f069655d1fb4fdee5b150591c88f40a0b2c132b4
+>>>>>>> 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
 export LD_PRELOAD=/tmp/preload.so
